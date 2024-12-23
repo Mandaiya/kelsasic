ef06957952e488668b3fda91268f804c9b491732
abitha000 <abithak000@yahoo.com>
2024-12-23 12:15:38 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -86,7 +86,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
 # https://www.gbmb.org/mb-to-bytes
 
-COOKIES = getenv("COOKIES", None)
+COOKIES = getenv("COOKIES", True)
 # https://batbin.me
 
 # config.py
